80ae5719e6dfc383a8b0d68b783931c35a3c3210
rianhasansiam <rianhasan1971@gmail.com>
2024-09-28 02:04:52 +0600

    mobile responsive done

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const ticktDetails = document.getElementById("ticktDetails");
 const indicator = document.getElementById("indicator");
 const availableSeat = document.getElementById("availableSeat");
+const cuponBtn=document.getElementById('cuponBtn')
+const cuponInput=document.getElementById('cuponInput');
+const totalPrice=document.getElementById('totalPrice');
+const discount=document.getElementById('discount');
+const grandTk=document.getElementById('grandTk');
+const reload=document.getElementById('reload');
 
+
+
+
+// button click
 let arr = [];
+let totalTk=0
+let vgrandTK=totalTk
+// let rian=['234','234','23']
 function busTicket(access) {
   const div = document.createElement("div");
+
   if (arr.includes(access.innerText)) {
     access.classList.toggle("bg-green-500");
     ticktDetails.removeChild(document.getElementById(access.innerText));
@@ -18,8 +32,9 @@ function busTicket(access) {
       return eachElement !== access.innerText;
       // function Nucleus
     });
-  } else {
-    if (arr.length >= 4) {
+  }
+   else {
+    if (arr.length >= 5) {
       alert("One person can booked maximum 4 tickets");
       return;
     }
@@ -33,11 +48,58 @@ function busTicket(access) {
         </div>`;
     ticktDetails.append(div);
     arr.push(access.innerText);
-
+    
+   
     const newAvaiableSeat = parseFloat(availableSeat.innerText) - 1;
     availableSeat.innerText = newAvaiableSeat;
 
     const newIndicatorSeat = parseFloat(indicator.innerText) + 1;
     indicator.innerText = newIndicatorSeat;
   }
+ 
+  totalTk=550*arr.length;
+  totalPrice.innerHTML=`${totalTk} BDT`
+  grandTk.innerText=`${totalTk} BDT`
+// btn disabled
+  if(arr.length > 3){
+    cuponBtn.removeAttribute('disabled');
+    
+    cuponInput.removeAttribute('disabled');
+
+    cuponBtn.classList.add('bg-green-500');
+    cuponBtn.classList.remove('bg-gray-500');
+    
+  }
+  else{
+    discount.classList.add('hidden');
+    cuponBtn.setAttribute("disabled",'')
+    cuponInput.setAttribute("disabled",'')
+    cuponBtn.classList.add('bg-gray-500');
+    cuponBtn.classList.remove('bg-green-500');
+  }
+
+  
+
 }
+  
+
+
+cuponBtn.addEventListener('click', function(){
+
+if(cuponInput.value !=="RIAN"){
+    
+  alert('Cupon Number not matching...!')
+  return cuponInput.value='';
+  
+  }
+  cuponInput.value=''
+  discount.classList.remove('hidden');
+  vgrandTK=totalTk-200;
+
+  grandTk.innerText=`${vgrandTK} BDT`
+
+})
+
+reload.addEventListener('click', function(){
+  window.location.reload();
+})
